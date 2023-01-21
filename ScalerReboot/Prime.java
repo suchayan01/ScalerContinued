3fab3c523037c0ef07c55f1461d4cf1c7388ee69
suchayan01 <suchayanbiswas@gmail.com>
2023-01-21 20:04:00 +0530

    simple prime checker

--- ScalerReboot/Prime.java
+++ ScalerReboot/Prime.java
@@ -1,4 +1,24 @@
 package ScalerReboot;
 
 public class Prime {
+    public static boolean check_prime(int num){
+        if (num==1) return false;
+        int factors_count=0;
+        for (int i=2;i*i<=num;i++){  // O(sqrt(n))
+            if (num%i==0){
+                if (num==num/i){
+                    factors_count++;
+                }
+                if (factors_count>1) return false;
+//                factors_count+=2;
+//                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int num =13;
+        System.out.println(check_prime(num));
+
+    }
 }
