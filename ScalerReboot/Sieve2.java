4a73f1990e6b5608165cdb0ea9718f996169652b
suchayan01 <suchayanbiswas@gmail.com>
2023-01-21 21:58:38 +0530

    Time complexity & space complexity addition

--- ScalerReboot/Sieve2.java
+++ ScalerReboot/Sieve2.java
@@ -3,16 +3,19 @@ package ScalerReboot;
 import java.util.Arrays;
 
 public class Sieve2 {
+    // TC = jmax*O(log(log(imax))= n* O(log(log(n)))= O(nlog(log(n))
+//    since for every i we are visiting jmax/2
+    // sc= O(n)
     public static void getPrime(int num){
         boolean prime_arr[]= new boolean [num+1];
         Arrays.fill(prime_arr,true);
         prime_arr[0]= prime_arr[1]= false;
-        for(int i=2;i<=num;i++){
+        for(int i=2;i<=num;i++){ // O(n)
             if (prime_arr[i]==false){
                 // do nothing
                 continue;
             }
-            for(int j=i*i;j<=num;j+=i){
+            for(int j=i*i;j<=num;j+=i){ // O(sqrt(n))
                 prime_arr[j]=false;
             }
         }
